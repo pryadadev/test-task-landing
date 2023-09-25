07fd262cc6810db694014c42e2db8deb10b2c29d
Mikhail Pryada <pryada16@gmail.com>
2023-09-25 11:59:02 +0300

    Updates

--- assets/arrow_down-5093b3bd.js
+++
@@ -1,3 +0,0 @@
-import{r as e}from"./index-87eca7b3.js";const r=t=>e.createElement("svg",{id:"Layer_1",xmlns:"http://www.w3.org/2000/svg",xmlnsXlink:"http://www.w3.org/1999/xlink",x:"0px",y:"0px",viewBox:"0 0 37 17.1",style:{enableBackground:"new 0 0 37 17.1"},xmlSpace:"preserve",...t},e.createElement("style",{type:"text/css"},`\r
-	.st0{fill:#FFFFFF;}\r
-`),e.createElement("path",{className:"st0",d:"M32.7,0.1L37,0.5L18.5,17.1L0,0.4L4.3,0l14.2,12.6L32.7,0.1z"})),a="/test-task-landing/assets/arrow_down-2f7a5d19.svg";export{r as ReactComponent,a as default};
