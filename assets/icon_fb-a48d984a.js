a391037f81ff610d03871e5217c1a7f5c8aa3ed0
Mikhail Pryada <pryada16@gmail.com>
2023-09-25 10:42:57 +0300

    Updates

--- assets/icon_fb-a48d984a.js
+++
@@ -1,3 +0,0 @@
-import{r as e}from"./index-75d6392e.js";const n=t=>e.createElement("svg",{id:"Layer_1",xmlns:"http://www.w3.org/2000/svg",xmlnsXlink:"http://www.w3.org/1999/xlink",x:"0px",y:"0px",viewBox:"0 0 42 42",style:{enableBackground:"new 0 0 42 42"},xmlSpace:"preserve",...t},e.createElement("style",{type:"text/css"},`\r
-	.st0{fill:#FFFFFF;}\r
-`),e.createElement("path",{className:"st0",d:"M23,16h2v-3h-2.4l0,0c-2.9,0.1-3.5,1.8-3.6,3.5l0,0V18h-2v3h2v8h3v-8h2.5l0.5-3h-3v-0.9C22,16.5,22.4,16,23,16z "})),a="/test-task-landing/assets/icon_fb-3f01f548.svg";export{n as ReactComponent,a as default};
