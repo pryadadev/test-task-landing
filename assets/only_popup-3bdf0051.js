85be728ce869cabfa9d1f85ce51f7f3ad052925b
Mikhail Pryada <pryada16@gmail.com>
2023-09-25 09:25:50 +0300

    Updates

---
+++ assets/only_popup-3bdf0051.js
@@ -0,0 +1,4 @@
+import{r as e}from"./index-75d6392e.js";const a=t=>e.createElement("svg",{id:"Layer_1",xmlns:"http://www.w3.org/2000/svg",xmlnsXlink:"http://www.w3.org/1999/xlink",x:"0px",y:"0px",viewBox:"0 0 161 36",style:{enableBackground:"new 0 0 161 36"},xmlSpace:"preserve",...t},e.createElement("style",{type:"text/css"},`\r
+	.st0{fill:#12B4B2;}\r
+	.st1{fill:#4DC3C6;}\r
+`),e.createElement("path",{className:"st0",d:"M10,25L0,24v12L10,25z"}),e.createElement("path",{className:"st1",d:"M0,0h161v26H0V0z"})),l="/test-task-landing/assets/only_popup-359e66c7.svg";export{a as ReactComponent,l as default};
